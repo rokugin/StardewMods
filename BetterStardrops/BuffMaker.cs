ffa8f3fe420cebd952eacb83d849ba79f47d7876
rokugin <rokugin@gmail.com>
2024-03-29 18:55:02 -0700

    Add better stardrops mod to repo

---
+++ BetterStardrops/BuffMaker.cs
@@ -0,0 +1,153 @@
+﻿using StardewValley.Buffs;
+using StardewValley;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterStardrops {
+    internal class BuffMaker {
+
+        internal Buff CreateAttackBuff(int amount) {
+            Buff newBuff = new Buff(
+                id: "{{ModId}}_StardropAttackBuff",
+                displayName: "Stardrop Attack Power",
+                description: "Additional attack from consuming stardrops",
+                duration: Buff.ENDLESS,
+                effects: new BuffEffects() {
+                    Attack = { amount }
+                }
+            );
+
+            return newBuff;
+        }
+
+        internal Buff CreateDefenseBuff(int amount) {
+            Buff newBuff = new Buff(
+                id: "{{ModId}}_StardropDefenseBuff",
+                displayName: "Stardrop Defense Power",
+                description: "Additional defense from consuming stardrops",
+                duration: Buff.ENDLESS,
+                effects: new BuffEffects() {
+                    Defense = { amount }
+                }
+            );
+
+            return newBuff;
+        }
+
+        internal Buff CreateImmunityBuff(int amount) {
+            Buff newBuff = new Buff(
+                id: "{{ModId}}_StardropImmunityBuff",
+                displayName: "Stardrop Immunity Power",
+                description: "Additional immunity from consuming stardrops",
+                duration: Buff.ENDLESS,
+                effects: new BuffEffects() {
+                    Immunity = { amount }
+                }
+            );
+
+            return newBuff;
+        }
+
+        internal Buff CreateCombatLevelBuff(int amount) {
+            Buff newBuff = new Buff(
+                id: "{{ModId}}_StardropCombatLevelBuff",
+                displayName: "Stardrop Combat Level Power",
+                description: "Additional combat skill level from consuming stardrops",
+                duration: Buff.ENDLESS,
+                effects: new BuffEffects() {
+                    CombatLevel = { amount }
+                }
+            );
+
+            return newBuff;
+        }
+
+        internal Buff CreateFarmingLevelBuff(int amount) {
+            Buff newBuff = new Buff(
+                id: "{{ModId}}_StardropFarmingLevelBuff",
+                displayName: "Stardrop Farming Level Power",
+                description: "Additional farming skill level from consuming stardrops",
+                duration: Buff.ENDLESS,
+                effects: new BuffEffects() {
+                    FarmingLevel = { amount }
+                }
+            );
+
+            return newBuff;
+        }
+
+        internal Buff CreateFishingLevelBuff(int amount) {
+            Buff newBuff = new Buff(
+                id: "{{ModId}}_StardropFishingLevelBuff",
+                displayName: "Stardrop Fishing Level Power",
+                description: "Additional fishing skill level from consuming stardrops",
+                duration: Buff.ENDLESS,
+                effects: new BuffEffects() {
+                    FishingLevel = { amount }
+                }
+            );
+
+            return newBuff;
+        }
+
+        internal Buff CreateForagingLevelBuff(int amount) {
+            Buff newBuff = new Buff(
+                id: "{{ModId}}_StardropForagingLevelBuff",
+                displayName: "Stardrop Foraging Level Power",
+                description: "Additional foraging skill level from consuming stardrops",
+                duration: Buff.ENDLESS,
+                effects: new BuffEffects() {
+                    ForagingLevel = { amount }
+                }
+            );
+
+            return newBuff;
+        }
+
+        internal Buff CreateLuckLevelBuff(int amount) {
+            Buff newBuff = new Buff(
+                id: "{{ModId}}_StardropLuckLevelBuff",
+                displayName: "Stardrop Luck Level Power",
+                description: "Additional luck skill level from consuming stardrops",
+                duration: Buff.ENDLESS,
+                effects: new BuffEffects() {
+                    LuckLevel = { amount }
+                }
+            );
+
+            return newBuff;
+        }
+
+        internal Buff CreateMiningLevelBuff(int amount) {
+            Buff newBuff = new Buff(
+                id: "{{ModId}}_StardropMiningLevelBuff",
+                displayName: "Stardrop Mining Level Power",
+                description: "Additional mining skill level from consuming stardrops",
+                duration: Buff.ENDLESS,
+                effects: new BuffEffects() {
+                    MiningLevel = { amount }
+                }
+            );
+
+            return newBuff;
+        }
+
+        internal Buff CreateMagneticBuff(int amount) {
+            Buff newBuff = new Buff(
+                id: "{{ModId}}_StardropMagneticBuff",
+                displayName: "Stardrop Magnetic Power",
+                description: "Additional magnetic radius from consuming stardrops",
+                duration: Buff.ENDLESS,
+                effects: new BuffEffects() {
+                    MagneticRadius = { amount }
+                }
+            );
+
+            return newBuff;
+        }
+
+    }
+}
---
+++ BetterStardrops/ModConfig.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterStardrops {
+    internal class ModConfig {
+
+        public bool ShowLogging { get; set; } = false;
+
+        public bool EnableAttack { get; set; } = false;
+        public int AttackIncreaseAmount { get; set; } = 5;
+
+        public bool EnableDefense { get; set; } = true;
+        public int DefenseIncreaseAmount { get; set; } = 3;
+
+        public bool EnableImmunity { get; set; } = true;
+        public int ImmunityIncreaseAmount { get; set; } = 3;
+
+        public bool EnableHealth { get; set; } = true;
+        public int HealthIncreaseAmount { get; set; } = 15;
+
+        public bool EnableCombatLevel { get; set; } = false;
+        public int CombatLevelIncreaseAmount { get; set; } = 1;
+
+        public bool EnableFarmingLevel { get; set; } = false;
+        public int FarmingLevelIncreaseAmount { get; set; } = 1;
+
+        public bool EnableFishingLevel { get; set; } = false;
+        public int FishingLevelIncreaseAmount { get; set; } = 1;
+
+        public bool EnableForagingLevel { get; set; } = false;
+        public int ForagingLevelIncreaseAmount { get; set; } = 1;
+
+        public bool EnableLuckLevel { get; set; } = false;
+        public int LuckLevelIncreaseAmount { get; set; } = 1;
+
+        public bool EnableMiningLevel { get; set; } = false;
+        public int MiningLevelIncreaseAmount { get; set; } = 1;
+
+        public bool EnableMagnetic { get; set; } = false;
+        public int MagneticIncreaseAmount { get; set; } = 1;
+
+    }
+}
