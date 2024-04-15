4d4d79f9cf6e230f75d8c005c3173b2538b7229b
rokugin <rokugin@gmail.com>
2024-04-15 13:31:13 -0700

    Add PassableDescents and StrongerTools to repo, Update CustomLocksUpdated manifest

---
+++ PassableDescents/ModEntry.cs
@@ -0,0 +1,54 @@
+﻿using HarmonyLib;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewValley;
+using StardewValley.Locations;
+
+namespace PassableDescents {
+    internal class ModEntry : Mod {
+
+        static IMonitor StaticMonitor;
+        static Vector2 tileVector2;
+
+        public override void Entry(IModHelper helper) {
+            StaticMonitor = Monitor;
+
+            var harmony = new Harmony(ModManifest.UniqueID);
+
+            harmony.Patch(
+                original: AccessTools.Method(typeof(MineShaft), nameof(MineShaft.createLadderDown)),
+                prefix: new HarmonyMethod(typeof(ModEntry), nameof(CreateLadderDownPrefix))
+                );
+            harmony.Patch(
+                original: AccessTools.Method(typeof(MineShaft), nameof(MineShaft.createLadderDown)),
+                postfix: new HarmonyMethod(typeof(ModEntry), nameof(CreateLadderDownPostfix))
+                );
+
+            harmony.Patch(
+                original: AccessTools.Method(typeof(MineShaft), nameof(MineShaft.createLadderAt)),
+                prefix: new HarmonyMethod(typeof(ModEntry), nameof(CreateLadderAtPrefix))
+                );
+            harmony.Patch(
+                original: AccessTools.Method(typeof(MineShaft), nameof(MineShaft.createLadderAt)),
+                postfix: new HarmonyMethod(typeof(ModEntry), nameof(CreateLadderAtPostfix))
+                );
+        }
+
+        static void CreateLadderDownPrefix(int x, int y) {
+            tileVector2 = new Vector2(x, y);
+        }
+
+        static void CreateLadderDownPostfix() {
+            Game1.currentLocation.Map.GetLayer("Buildings").Tiles[(int)tileVector2.X, (int)tileVector2.Y].Properties.Add("Passable", "T");
+        }
+
+        static void CreateLadderAtPrefix(Vector2 p) {
+            tileVector2 = p;
+        }
+
+        static void CreateLadderAtPostfix() {
+            Game1.currentLocation.Map.GetLayer("Buildings").Tiles[(int)tileVector2.X, (int)tileVector2.Y].Properties.Add("Passable", "T");
+        }
+
+    }
+}
---
+++ StrongerTools/ModEntry.cs
@@ -0,0 +1,112 @@
+﻿using StardewModdingAPI;
+using StardewValley.Tools;
+using StardewValley;
+using GenericModConfigMenu;
+using StardewValley.TerrainFeatures;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using StardewValley.Locations;
+using xTile.Tiles;
+using HarmonyLib;
+using xTile.ObjectModel;
+
+namespace StrongerTools {
+    internal class ModEntry : Mod {
+
+        Item item;
+        ModConfig config = new();
+        
+        public override void Entry(IModHelper helper) {
+            config = helper.ReadConfig<ModConfig>();
+            
+            helper.Events.GameLoop.GameLaunched += OnGameLaunched;
+            helper.Events.Input.ButtonPressed += OnButtonPressed;
+            helper.Events.GameLoop.DayStarted += OnDayStarted;
+        }
+
+        private void OnDayStarted(object? sender, StardewModdingAPI.Events.DayStartedEventArgs e) {
+            GameLocation location = Game1.getLocationFromName("Backwoods");
+            
+            AddTerrainFeature(location, new Vector2(34, 10));
+            AddTerrainFeature(location, new Vector2(41, 16));
+            AddTerrainFeature(location, new Vector2(12, 19));
+            AddTerrainFeature(location, new Vector2(15, 17));
+            AddTerrainFeature(location, new Vector2(20, 20));
+            AddTerrainFeature(location, new Vector2(25, 16));
+            AddTerrainFeature(location, new Vector2(17, 14));
+            AddTerrainFeature(location, new Vector2(19, 12));
+            AddTerrainFeature(location, new Vector2(21, 11));
+            AddTerrainFeature(location, new Vector2(28, 9));
+            AddTerrainFeature(location, new Vector2(46, 12));
+            Game1.netWorldState.Value.canDriveYourselfToday.Value = true;
+        }
+
+        void AddTerrainFeature(GameLocation location, Vector2 tilePosition) {
+            if (!location.IsTileOccupiedBy(tilePosition)) {
+                location.terrainFeatures.Add(tilePosition, new Tree(Tree.pineTree, Tree.treeStage));
+                if (config.ShowLogs) Monitor.Log($"Placing stage 5 pine tree at {location.Name} (X:{tilePosition.X}, Y:{tilePosition.Y})", LogLevel.Info);
+            }
+        }
+
+        private void OnGameLaunched(object? sender, StardewModdingAPI.Events.GameLaunchedEventArgs e) {
+            var configMenu = Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
+            if (configMenu is null) return;
+
+            // register mod
+            configMenu.Register(
+                mod: ModManifest,
+                reset: () => config = new ModConfig(),
+                save: () => Helper.WriteConfig(config)
+            );
+
+            configMenu.AddSectionTitle(
+                mod: ModManifest,
+                text: () => "Debug"
+            );
+            configMenu.AddBoolOption(
+                mod: ModManifest,
+                getValue: () => config.ShowLogs,
+                setValue: value => config.ShowLogs = value,
+                name: () => "Enabled"
+            );
+            
+            //EditAudio("thunder", "mySound.wav", "Sound");
+            //EditAudio("thunder_small", "mySound.wav", "Sound");
+        }
+
+        void EditAudio(string cue, string audioFile, string category) {
+            var existingCueDef = Game1.soundBank.GetCueDefinition(cue);
+            SoundEffect audio;
+            string filePathCombined = Path.Combine(Helper.DirectoryPath, audioFile);
+
+            using (var stream = new FileStream(filePathCombined, FileMode.Open)) {
+                audio = SoundEffect.FromStream(stream);
+            }
+
+            existingCueDef.SetSound(audio, Game1.audioEngine.GetCategoryIndex(category), false);
+        }
+
+        private void OnButtonPressed(object? sender, StardewModdingAPI.Events.ButtonPressedEventArgs e) {
+            if (!Context.IsPlayerFree) return;
+            if (Game1.activeClickableMenu != null) return;
+
+            item = Game1.player.CurrentTool;
+            
+            if (e.Button.IsUseToolButton()) {
+                if (item is Pickaxe pickaxe) {
+                    switch (pickaxe.ItemId) {
+                        case "GoldPickaxe":
+                            if (pickaxe.additionalPower.Value < 3) pickaxe.additionalPower.Value = 3;
+                            break;
+                        case "IridiumPickaxe":
+                            if (pickaxe.additionalPower.Value < 10) pickaxe.additionalPower.Value = 10;
+                            break;
+                    }
+
+                    if (config.ShowLogs) Monitor.Log($"{pickaxe.Name} current additional power: +{pickaxe.additionalPower.Value}", LogLevel.Info);
+                }
+            }
+        }
+
+    }
+}
