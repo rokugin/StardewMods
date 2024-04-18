01f95906c6ac1b9f1f00f89b612fc078d0c4ffb6
rokugin <rokugin@gmail.com>
2024-04-18 10:36:30 -0700

    Modify tooltip for PassableDescents delay setting, Add SafeLightningUpdated mod to repo

---
+++ SafeLightningUpdated/SafeLightningUpdated/ModConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace SafeLightningUpdated {
+    internal class ModConfig {
+
+        public bool ModEnabled { get; set; } = true;
+
+        public bool DisableStrikes { get; set; } = true;
+
+    }
+}
