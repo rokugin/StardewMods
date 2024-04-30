2d6b81eb19cdece3d70fb1f1368ba36a386c0771
rokugin <rokugin@gmail.com>
2024-04-30 12:49:37 -0700

    Add check for treat as outdoors, Remove unused event

--- NoIndoorDismount/ModEntry.cs
+++ NoIndoorDismount/ModEntry.cs
@@ -15,19 +15,25 @@ namespace NoIndoorDismount {
             helper.Events.GameLoop.GameLaunched += OnGameLaunched;
 
             var harmony = new Harmony(ModManifest.UniqueID);
-            
+
             harmony.Patch(
                 original: AccessTools.Method(typeof(Game1), nameof(Game1.ShouldDismountOnWarp)),
+                prefix: new HarmonyMethod(typeof(ModEntry), nameof(Game1_ShouldDismountOnWarp_Prefix)),
                 postfix: new HarmonyMethod(typeof(ModEntry), nameof(Game1_ShouldDismountOnWarp_Postfix))
             );
         }
 
-        static void Game1_ShouldDismountOnWarp_Postfix(ref bool __result) {
-            __result = false;
+        static void Game1_ShouldDismountOnWarp_Prefix(GameLocation new_location, out GameLocation __state) {
+            __state = new_location;
+        }
+
+        static void Game1_ShouldDismountOnWarp_Postfix(ref bool __result, GameLocation __state) {
+            bool dismount = __state.IsOutdoors || __state.treatAsOutdoors.Value;
+            __result = !dismount;
         }
 
         private void OnGameLaunched(object? sender, StardewModdingAPI.Events.GameLaunchedEventArgs e) {
-            
+
         }
 
     }
