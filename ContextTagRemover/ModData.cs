61fe26516def0cb631d31e9027e6d68e4ae42b0a
rokugin <rokugin@gmail.com>
2024-06-28 04:11:20 -0700

    Remove mystery boxes until 1.6.9, Begin adding framework for CP editable data

---
+++ ContextTagRemover/ModData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContextTagRemover {
+    internal record ModData {
+
+        public Dictionary<string, string[]> ObjectTags { get; }
+
+        public ModData(Dictionary<string, string[]> objectTags) {
+            ObjectTags = objectTags ?? new();
+        }
+
+    }
+}
---
+++ ContextTagRemover/TagRemoverContentLoader.cs
@@ -0,0 +1,18 @@
+﻿using StardewModdingAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContextTagRemover {
+    internal class TagRemoverContentLoader {
+
+        readonly string objectsAssetName;
+
+        readonly ModData modData;
+
+        readonly IModRegistry modRegistry;
+
+    }
+}
